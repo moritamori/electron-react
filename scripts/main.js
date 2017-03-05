04ce131f90d1c11a069c98bb41201ec0c6b71700
tamo <tamo@sonicgarden.jp>
2017-03-05 16:30:09 +0900

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Main from '../views/main.jsx';
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('content')
+);
